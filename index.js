d7817b8e55abb4f3823031be98750ee193031097
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-01 07:16:49 +0300

    Completed practice

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let count = 1;
+let count = 0;
 while (count < 5) {
-  if (count === 1) {
+  debbuger;
+  if (count === 1 || count === 0) {
     console.log(`The loop has executed ${count} time.`);
   } else {
     console.log(`The loop has executed ${count} times.`);
   }
   count += 1;
+  debbuger;
 }
